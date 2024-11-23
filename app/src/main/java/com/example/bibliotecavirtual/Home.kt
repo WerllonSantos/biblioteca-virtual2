e4caff54bf77846882aae3c890fd826d5c35d1d6
WerllonSantos <paulowerllon@hotmail.com>
2024-11-23 00:32:09 -0300

    ajuste

--- app/src/main/java/com/example/bibliotecavirtual/Home.kt
+++ app/src/main/java/com/example/bibliotecavirtual/Home.kt
@@ -45,23 +45,7 @@ class Home : AppCompatActivity() {
 
 
         checkLocationPermission()
-
-
-        val livrosPopulares = listOf("Livro 1", "Livro 2", "Livro 3", "Livro 4", "Livro 5")
-        val novidades = listOf("Novo 1", "Novo 2", "Novo 3", "Novo 4", "Novo 5")
-        val recomendados = listOf("Recomendado 1", "Recomendado 2", "Recomendado 3", "Recomendado 4")
-
-
-        configurarRecyclerView(findViewById(R.id.recyclerView1), livrosPopulares)
-        configurarRecyclerView(findViewById(R.id.recyclerView2), novidades)
-        configurarRecyclerView(findViewById(R.id.recyclerView3), recomendados)
-    }
-
-
-    private fun configurarRecyclerView(recyclerView: RecyclerView, data: List<String>) {
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        recyclerView.adapter = BookAdapter(this, data)
-    }
+  }
 
 
     private fun checkLocationPermission() {
--- app/src/main/java/com/example/bibliotecavirtual/activity_book_detail.java
+++
@@ -1,6 +0,0 @@
-package com.example.bibliotecavirtual;
-
-import android.app.Activity;
-
-public class activity_book_detail extends Activity {
-}
